9fc7ee9310839a0f096d3949d7a524d2c5715f53
RahulChettiyar05 <rahulgopu5@gmail.com>
2025-11-28 10:59:24 +0530

    Practising Basics

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 
-// Example of ternary operator
+/* Example of ternary operator
 
 let score = 10;
 let grade = score>50 ? "Pass" : "Fail" ;
 
 console.log(grade)
+*/
+
+// JavaScript Practice Basic level questions
+
+/*
+#1 Write a program to add two numbers and print the result.
+
+let a = parseInt(prompt("Enter a number"));
+let b = parseInt(prompt("Enter 2nd number"));
+let sum = a+b;
+console.log(sum);
+
+
+#2 Check whether a number is even or odd using the modulus (%) operator
+
+let a = parseInt(prompt("Enter a number"));
+
+if(a%2==0) {
+    console.log("Even Number");
+}
+else {
+    console.log("Odd Number");
+}
+
+#3 Given let a = 10; let b = 20;, use comparison operators to check:
+Is a > b?
+Is a == b?
+Is a != b?
+
+
+let a = 10;
+let b = 20;
+
+console.log(a>b);
+console.log(a==b);
+console.log(a!=b);
+*/
